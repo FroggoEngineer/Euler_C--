f7e4f8351c5919dd6f1d3feb46b5cbf54f86e12f
kevso311 <kevso311@student.liu.se>
2018-01-02 16:17:21 +0100

    Problem 6

---
+++ e6.cpp
@@ -0,0 +1,20 @@
+/*
+#include <iostream>
+
+int main() {
+
+	int squaredSum{ 0 };
+	int sumOfSquares{ 0 };
+
+	for (int i{ 1 }; i <= 100; ++i) {
+		squaredSum += i;
+		sumOfSquares += (i*i);
+	}
+
+	squaredSum *= squaredSum;
+
+	std::cout << squaredSum - sumOfSquares << std::endl;
+	system("pause");
+}
+
+*/
