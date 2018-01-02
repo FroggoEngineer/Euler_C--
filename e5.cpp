7140e59c81c10bfb297a02ce71a3b5028c126fcf
kevso311 <kevso311@student.liu.se>
2018-01-02 16:13:34 +0100

    Problem 5

---
+++ e5.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+
+int main() {
+
+	int num{ 0 };
+	int counter{ 2 };
+	
+	while (true) {
+		num = 20*counter;
+
+		bool found{ true };
+		for (int i{ 1 }; i <= 20; ++i) {
+			if (num % i != 0) {
+				found = false;
+				break;
+			}
+				
+		}
+
+		if (found) {
+			break;
+		}
+
+		counter++;
+	}
+
+	std::cout << num << std::endl;
+	system("pause");
+
+}
+*/
